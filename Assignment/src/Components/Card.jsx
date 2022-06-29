402f1069f9855ed50a71e5bedae221a123b8d39d
shubhampandey <72190363+bugscreater@users.noreply.github.com>
2022-06-30 02:49:18 +0530

    Update Card.jsx

--- Assignment/src/Components/Card.jsx
+++ Assignment/src/Components/Card.jsx
@@ -1,66 +1,49 @@
-import React,{useState} from 'react'
-
-
-
+import React, { useState } from "react";
 
 function Card(props) {
-   
-   
-    const dragStart = e =>{
-        let prev_card_id = localStorage.getItem("selected_card");
-        let prev_card = document.getElementById(prev_card_id);
-       
-        setTimeout(()=>{
-          prev_card.style.display = "block";
-        
-        },100);
-       
-        localStorage.clear();
-        
-        const target = e.target;
-        e.dataTransfer.setData('card_id',target.id);
-        
-        
-        
-        setTimeout(()=>{
-          props.setTag(props.value);
-        
-        },100);
-        
-        setTimeout(()=>{
-            target.style.display = "none";
-          
-        },100);
-       
-        localStorage.setItem("selected_card",target.id);
-       
-    }
-    
+  const dragStart = (e) => {
+    let prev_card_id = localStorage.getItem("selected_card");
+    let prev_card = document.getElementById(prev_card_id);
 
-    const dragOver = e =>{
-       
-        
-        e.stopPropagation();
-
+    if (prev_card !== null) {
+      setTimeout(() => {
+        prev_card.style.display = "block";
+      }, 100);
     }
 
+    localStorage.clear();
 
+    const target = e.target;
+    e.dataTransfer.setData("card_id", target.id);
 
-    return (
-      <> 
+    setTimeout(() => {
+      props.setTag(props.value);
+    }, 100);
+
+    setTimeout(() => {
+      target.style.display = "none";
+    }, 100);
+
+    localStorage.setItem("selected_card", target.id);
+  };
+
+  const dragOver = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <>
       <div
-        id = {props.id}
-        onDragStart = {dragStart}
-        onDragOver = {dragOver}
-        className = {props.className}
-        draggable = "true"
+        id={props.id}
+        onDragStart={dragStart}
+        onDragOver={dragOver}
+        className={props.className}
+        draggable="true"
       >
-       {props.children}
+        {props.children}
       </div>
-      
-      </> 
-
-    )
+    </>
+  );
 }
 
-export default Card
+export default Card;

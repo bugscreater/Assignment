86bd21cf04eb2bf7b8650da5f098efb25e385d97
shubhampandey <72190363+bugscreater@users.noreply.github.com>
2022-06-30 02:50:17 +0530

    Update Result.jsx

--- Assignment/src/Components/Result.jsx
+++ Assignment/src/Components/Result.jsx
@@ -1,32 +1,25 @@
-import React from 'react'
-import { useEffect,useState} from 'react';
-import {useLocation} from 'react-router-dom';
+import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function Result() {
   const location = useLocation();
-  
-  const[tag,setTag] = useState(location.state.tag);
-  const[data,setData] = useState(location.state.data);
+  const data = location.state.data;
 
-  useEffect(()=>{
-    document.getElementById('content').innerHTML = ""
-    let text  =  document.createElement( `${tag}`); 
-    var context = document.createTextNode(data);
-    text.appendChild(context);
-
-    document.getElementById('content').appendChild(text);
-    
-  },[])
   
+  
+ 
+
   
 
   return (
-      <div id = "content">
-        
-         
-         
-      </div>
-  ) 
+    <div id="content">
+        <div
+       id="result" className="result"
+       dangerouslySetInnerHTML={{__html: data}}
+      />
+    </div>
+  );
 }
 
-export default Result
+export default Result;
